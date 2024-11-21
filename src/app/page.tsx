4137693cb379d6cea1c3a1bfac26d76757e78ebe
nceplayindia <niceplayindia@gmail.com>
2024-11-21 23:08:39 +0530

    Google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,24 @@
 // src/app/page.tsx
 
-import { signIn, useSession } from "next-auth/react";
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";  // Import signIn, signOut, and useSession
 import Image from "next/image";
 
 export default function Home() {
   const { data: session } = useSession();
 
   return (
-    <div className="flex justify-center items-center h-screen flex-col">
-      {session ? (
-        <>
-          <h1>Welcome, {session.user?.name}!</h1>
-          {/* Use Image component for better performance */}
-          <Image 
-            src={session.user?.image || "/default-image.jpg"} 
-            alt="User image" 
-            width={100} 
-            height={100} 
-          />
-          <button onClick={() => signOut()}>Sign out</button>
-        </>
+    <>
+      <h1>Hello From LooperCode!!</h1>
+      {!session ? (
+        <button onClick={() => signIn("google")}>Sign in with Google</button>
       ) : (
         <>
-          <h1>Hello From LooperCode!!!!</h1>
-          <button
-            onClick={() => signIn("google")}
-            className="bg-blue-500 text-white p-2 rounded mt-4"
-          >
-            Sign in with Google
-          </button>
+          <p>Welcome, {session.user?.name}</p>
+          <button onClick={() => signOut()}>Sign out</button>  {/* Use signOut here */}
         </>
       )}
-    </div>
+    </>
   );
 }
