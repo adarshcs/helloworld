6746128c3a67900c25267b1307405d3e63909975
nceplayindia <niceplayindia@gmail.com>
2024-11-21 22:43:47 +0530

    Google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,27 @@
+import { signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
   return (
-    <div className="flex justify-center items-center h-screen">
-      <h1 className="text-4xl">Hello from LooperCode!!</h1>
+    <div className="flex justify-center items-center h-screen flex-col">
+      {session ? (
+        <>
+          <h1>Welcome, {session.user?.name}!</h1>
+          <img src={session.user?.image} alt="User image" />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      ) : (
+        <>
+          <h1>Hello From LooperCode!!</h1>
+          <button
+            onClick={() => signIn("google")}
+            className="bg-blue-500 text-white p-2 rounded mt-4"
+          >
+            Sign in with Google
+          </button>
+        </>
+      )}
     </div>
   );
 }
