1953dc2ca6be72c043b1e231b3dbd3b83f1bca2e
nceplayindia <niceplayindia@gmail.com>
2024-11-21 22:14:19 +0530

    Text alignment modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl">Hello from LooperCode!!</h1>
+    </div>
+  );
 }
