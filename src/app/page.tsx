ee49d228b94de943e7d3e635c3796a3db37dc3c1
nceplayindia <niceplayindia@gmail.com>
2024-11-21 23:11:48 +0530

    Google sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,12 @@
 import { signIn, signOut, useSession } from "next-auth/react";  // Import signIn, signOut, and useSession
 
 export default function Home() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  // Check if the session data is still loading
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
